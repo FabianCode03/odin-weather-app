5577a829296154bb3b63b4886d350beebc7b39b7
Fabian_PC <fb-224651@hs-weingarten.de>
2024-08-11 00:48:42 +0200

    Add and test getWeather module

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import './style.css';
 import { Header } from './components/Header';
+import { getWeather } from './modules/getWeather';
 
 const app = document.getElementById('app');
 const header = Header();
@@ -7,3 +8,7 @@ const header = Header();
 if (app) {
   app.appendChild(header);
 }
+
+// getWeather('friedrichshafen')
+//   .then((msg) => console.log(msg))
+//   .catch((err) => console.error(err.message));
--- src/modules/exampleModule.ts
+++
@@ -1,5 +0,0 @@
-// standard webpack exampleModule.ts
-
-export default function exampleModule(someArg: string): string {
-  return `This is an example module with arg: ${someArg}`;
-}
