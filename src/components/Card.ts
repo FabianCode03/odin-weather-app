176c63628f1cab524f3ef05bf61f9d593c9fcaef
Fabian_PC <fb-224651@hs-weingarten.de>
2024-08-13 22:00:33 +0200

    Implement and style Card component

---
+++ src/components/Card.ts
@@ -0,0 +1,33 @@
+export function Card(
+  title: string,
+  infoCorner: string,
+  body: HTMLElement,
+): HTMLElement {
+  // create elements
+  const card = document.createElement('div');
+  const cardHeader = document.createElement('div');
+  const cardTitle = document.createElement('h2');
+  const cardInfoCorner = document.createElement('div');
+  const cardBody = document.createElement('div');
+
+  // add content
+  cardTitle.textContent = title;
+  cardInfoCorner.textContent = infoCorner;
+  cardBody.appendChild(body);
+
+  // add classes
+  card.classList.add('card');
+  cardHeader.classList.add('card-header');
+  cardTitle.classList.add('card-title');
+  cardInfoCorner.classList.add('card-info-corner');
+  cardBody.classList.add('card-body');
+
+  // assemble
+  cardHeader.appendChild(cardTitle);
+  cardHeader.appendChild(cardInfoCorner);
+  card.appendChild(cardHeader);
+
+  card.appendChild(cardBody);
+
+  return card;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,15 @@
 import './style.css';
 import { Header } from './components/Header';
-import { getWeather } from './modules/getWeather';
-import { extractWeatherData } from './modules/extractWeatherData';
-import { switchTempUnit } from './utils/switchTempUnit';
-import { CurrentWeather } from './components/CurrentWeather';
+import { Card } from './components/Card';
 
 const app = document.getElementById('app');
 const header = Header();
+const cardBodyExample = document.createElement('div');
+cardBodyExample.textContent = 'This is an example card body.';
+
+const card = Card('Current Weather', '12:52', cardBodyExample);
 
 if (app !== null) {
   app.appendChild(header);
+  app.appendChild(card);
 }
-
-const weather = getWeather('friedrichshafen');
-weather
-  .then((data) => {
-    const weatherData = extractWeatherData(data);
-    const weatherInCelsius = switchTempUnit(weatherData);
-    const currentWeather = new CurrentWeather(weatherInCelsius);
-    const weatherWidget = document.createElement('div');
-    weatherWidget.innerHTML = currentWeather.render();
-    if (app !== null) {
-      app.appendChild(weatherWidget);
-    }
-  })
-  .catch((error) => {
-    console.error('Error fetching weather data:', error);
-  });
