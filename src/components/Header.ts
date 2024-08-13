18942b7297f66a42ce97f54f2acc51a26f035663
Fabian_PC <fb-224651@hs-weingarten.de>
2024-08-13 21:25:35 +0200

    Implement Header component

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,17 +1,29 @@
+import logo from '../img/weatherrino_logo.png';
+import searchIcon from '../img/searchIcon.svg';
+
 export function Header(): HTMLElement {
   // create elements
   const header = document.createElement('header');
+  const logoContainer = document.createElement('div');
+  const searchIconElement = document.createElement('img');
   const title = document.createElement('h1');
 
   // add content
-  title.textContent = 'Wheatherrino';
+  title.textContent = 'Weatherrhino';
+  logoContainer.style.backgroundImage = `url(${logo})`;
+  searchIconElement.src = searchIcon;
+  searchIconElement.alt = 'Search icon';
+  searchIconElement.classList.add('search-icon-container');
 
   // add classes
   header.classList.add('header');
+  logoContainer.classList.add('logo-container');
   title.classList.add('title');
 
   // assemble
+  header.appendChild(logoContainer);
   header.appendChild(title);
+  header.appendChild(searchIconElement);
 
   return header;
 }
