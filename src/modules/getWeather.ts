cbad2fecef4f876f5665080f9375ec1321d57e65
Fabian_PC <fb-224651@hs-weingarten.de>
2024-08-18 19:46:42 +0200

    Implement top level error handling in getWeather

--- src/modules/getWeather.ts
+++ src/modules/getWeather.ts
@@ -2,16 +2,69 @@ import { fetchWeather } from './fetchWeather';
 import { extractWeatherData } from './extractWeatherData';
 import { switchTempUnit } from '../utils/switchTempUnit';
 import type { WeatherData } from '../types/weatherTypes';
+import { Err, Ok } from 'ts-results';
+import type { Result } from 'ts-results';
+import {
+  type GetWeatherError,
+  CityNotFoundError,
+  DataParsingError,
+  NetworkError,
+  UnknownError,
+} from '../errors/getWeatherError';
 
-export async function getWeather(city: string): Promise<WeatherData> {
-  try {
-    const data = await fetchWeather(city);
-    const weatherData = extractWeatherData(data);
-    return switchTempUnit(weatherData);
-  } catch (error) {
-    if ((error as Error).message === 'City not found') {
-      throw new Error('City not found');
-    }
-    throw error;
+import {
+  BadRequestError,
+  UnauthorizedError,
+  NotFoundError,
+  TooManyRequestsError,
+  InternalServerError,
+  UnknownFetchError,
+  JSONParsingError,
+} from '../errors/fetchWeatherError';
+
+export async function getWeather(city: string): Promise<Result<WeatherData, GetWeatherError>> {
+  const data = await fetchWeather(city);
+
+  // Valid endpoint but invalid parameters -> 400 Bad Request -> CityNotFoundError
+  if (data.err && data.val instanceof BadRequestError) {
+    return Err(new CityNotFoundError(data.val));
   }
+
+  // API key or subscription problem -> 401 Unauthorized -> NetworkError
+  if (data.err && data.val instanceof UnauthorizedError) {
+    return Err(new NetworkError(data.val));
+  }
+
+  // No valid endpoint for request -> 404 Not Found -> NetworkError
+  if (data.err && data.val instanceof NotFoundError) {
+    return Err(new NetworkError(data.val));
+  }
+
+  // Too Many Requests -> 429 Too Many Requests -> NetworkError
+  if (data.err && data.val instanceof TooManyRequestsError) {
+    return Err(new NetworkError(data.val));
+  }
+
+  // Internal server error -> 500 Internal Server Error -> NetworkError
+  if (data.err && data.val instanceof InternalServerError) {
+    return Err(new NetworkError(data.val));
+  }
+
+  // Error parsing JSON -> JSONParsingError
+  if (data.err && data.val instanceof JSONParsingError) {
+    return Err(new DataParsingError(data.val));
+  }
+
+  // Unknown error -> UnknownError
+  if (data.err && data.val instanceof UnknownFetchError) {
+    return Err(new UnknownError(data.val));
+  }
+
+  const weatherData = extractWeatherData(data.val);
+
+  if (weatherData.err) {
+    return Err(new DataParsingError(weatherData.val));
+  }
+
+  return Ok(switchTempUnit(weatherData.val));
 }
