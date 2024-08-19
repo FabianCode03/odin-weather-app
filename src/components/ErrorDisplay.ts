f0b676b2fe0889e77c34993bce4217d9fb244290
Fabian_PC <fb-224651@hs-weingarten.de>
2024-08-19 03:35:39 +0200

    Implement and style ErrorDisplay component

---
+++ src/components/ErrorDisplay.ts
@@ -0,0 +1,44 @@
+import { Card } from './Card';
+import { type GetWeatherError } from '../errors/getWeatherError';
+import { FetchWeatherError } from '../errors/fetchWeatherError';
+
+export function ErrorDisplay(error: GetWeatherError): HTMLElement {
+  // checking which element to display
+  const hasOriginalError = error.originalError != null;
+  const optionalHttpStatusCode: number | null =
+    hasOriginalError && error.originalError instanceof FetchWeatherError && error.originalError.httpStatus != null
+      ? error.originalError.httpStatus
+      : null;
+
+  // create elements
+  const cardBody = document.createElement('div');
+  const userFriendlyMessage = document.createElement('p');
+  const detailedMessage = document.createElement('p');
+
+  // add classes
+  cardBody.classList.add('card-body', 'error-body');
+  userFriendlyMessage.classList.add('user-friendly-message', 'separator');
+  detailedMessage.classList.add('detailed-message');
+
+  // add content
+  userFriendlyMessage.textContent = error.userFriendlyMessage;
+  detailedMessage.textContent = error.originalError != null ? error.originalError.message : '';
+
+  // assemble
+  cardBody.appendChild(userFriendlyMessage);
+  if (hasOriginalError) {
+    cardBody.appendChild(detailedMessage);
+  }
+
+  // Append status code to error name if it exists
+  const errorNameWithStatusCode =
+    optionalHttpStatusCode != null ? `${error.name} (${optionalHttpStatusCode})` : error.name;
+
+  const errorDisplay = Card('Error', errorNameWithStatusCode, cardBody);
+
+  errorDisplay.id = 'error';
+  errorDisplay.classList.add('error-card');
+  cardBody.lastElementChild?.classList.remove('separator');
+
+  return errorDisplay;
+}
