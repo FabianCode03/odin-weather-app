5cc50a2694216443100ff3adabce95d72e45e1d7
Fabian_PC <fb-224651@hs-weingarten.de>
2024-08-20 14:36:06 +0200

    Add and style TodaysDetails component, grid layout
    - grid now shows 1 column on mobile, 2 on tablet,
      and 4 on bigger screens.
    - in configuration 2, an interesting layout is
      achieved by moving 2 cards temporarily to the
      bottom of the grid.
    - added a new component TodaysDetails to show the

---
+++ src/components/TodaysDetails.ts
@@ -0,0 +1,37 @@
+import type { WeatherData } from '../types/weatherTypes';
+import { Card } from './Card';
+import { InfoRow } from './InfoRow';
+import minMaxTemp from '../img/ui_icons/minMaxTemp.png';
+import temp from '../img/ui_icons/temp.png';
+import humidity from '../img/ui_icons/humidity.png';
+
+export function Today(weather: WeatherData): HTMLElement {
+  // create elements
+  const cardBody = document.createElement('div');
+
+  // add classes
+  cardBody.classList.add('today-body');
+
+  // create info rows
+  const temperature = InfoRow('Temperature', `${weather.days[0].temp.toFixed(0)}${weather.tempUnit}`, temp);
+  const minMax = InfoRow(
+    'Min/Max',
+    `${weather.days[0].tempmin.toFixed(0)}${weather.tempUnit} / ${weather.days[0].tempmax.toFixed(0)}${weather.tempUnit}`,
+    minMaxTemp,
+  );
+  const humidityElement = InfoRow('Humidity', `${weather.days[0].humidity}%`, humidity);
+
+  // assemble
+  cardBody.appendChild(temperature);
+  cardBody.appendChild(minMax);
+  cardBody.appendChild(humidityElement);
+  cardBody.lastElementChild?.classList.remove('separator');
+  (cardBody.firstElementChild as HTMLElement)?.style.setProperty('padding-top', '0px');
+
+  // Extract month and day from the date string
+  const dateWithoutYear = weather.days[0].datetime.slice(0, 7);
+
+  const todayCard = Card("Today's Details", `${dateWithoutYear}📅`, cardBody);
+  todayCard.id = 'today';
+  return todayCard;
+}
--- src/modules/loadWidgets.ts
+++ src/modules/loadWidgets.ts
@@ -2,6 +2,7 @@ import { CurrentWeather } from '../components/CurrentWeather';
 import { SolarCycle } from '../components/SolarCycle';
 import { Forecast } from '../components/Forecast';
 import { HoursPreview } from '../components/HoursPreview';
+import { Today } from '../components/TodaysDetails';
 import { type Hour, type WeatherData } from '../types/weatherTypes';
 import { getNext24Hours } from '../utils/getNext24Hours';
 
@@ -11,9 +12,12 @@ export function loadWidgets(body: HTMLElement, weather: WeatherData): void {
   const currentWeather = CurrentWeather(weather);
   const solarCycle = SolarCycle(weather.currentConditions.sunrise, weather.currentConditions.sunset);
   const forecast = Forecast(weather.description);
+  const today = Today(weather);
 
   body.appendChild(currentWeather);
-  body.appendChild(solarCycle);
+  body.appendChild(today);
   body.appendChild(forecast);
+
+  body.appendChild(solarCycle);
   body.appendChild(hoursPreview);
 }
