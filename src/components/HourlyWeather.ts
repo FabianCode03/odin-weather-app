7fb7264c94d56da64b3a9b2777a89c6f9aa6bce9
Fabian_PC <fb-224651@hs-weingarten.de>
2024-08-20 15:26:33 +0200

    Rename HoursPreview to HourlyWeather

--- src/components/HourlyWeather.ts
+++ src/components/HourlyWeather.ts
@@ -2,12 +2,12 @@ import type { Hour } from '../types/weatherTypes';
 import { Card } from './Card';
 import { HourColumn } from './HourColumn';
 
-export function HoursPreview(hours: Hour[]): HTMLElement {
+export function HourlyWeather(hours: Hour[]): HTMLElement {
   //   create elements
   const cardBody = document.createElement('div');
 
   //   set classes
-  cardBody.classList.add('hours-preview-body');
+  cardBody.classList.add('hourly-weather-body');
 
   //   create hour rows
   const hourRows = hours.map((hour) => HourColumn(hour));
@@ -15,7 +15,7 @@ export function HoursPreview(hours: Hour[]): HTMLElement {
   //   assemble
   cardBody.append(...hourRows);
   cardBody.lastElementChild?.classList.remove('separator');
-  const hoursPreview = Card('Hours Preview', '⌚', cardBody);
+  const hoursPreview = Card('Hourly Weather', '⌚', cardBody);
   hoursPreview.id = 'hours-preview';
   return hoursPreview;
 }
--- src/modules/loadWidgets.ts
+++ src/modules/loadWidgets.ts
@@ -1,14 +1,14 @@
 import { CurrentWeather } from '../components/CurrentWeather';
 import { SolarCycle } from '../components/SolarCycle';
 import { Forecast } from '../components/Forecast';
-import { HoursPreview } from '../components/HoursPreview';
+import { HourlyWeather } from '../components/HourlyWeather';
 import { Today } from '../components/TodaysDetails';
 import { type Hour, type WeatherData } from '../types/weatherTypes';
 import { getNext24Hours } from '../utils/getNext24Hours';
 
 export function loadWidgets(body: HTMLElement, weather: WeatherData): void {
   const hoursArray: Hour[] = getNext24Hours(weather);
-  const hoursPreview = HoursPreview(hoursArray);
+  const hoursPreview = HourlyWeather(hoursArray);
   const currentWeather = CurrentWeather(weather);
   const solarCycle = SolarCycle(weather.currentConditions.sunrise, weather.currentConditions.sunset);
   const forecast = Forecast(weather.days[0].description, weather.description);
